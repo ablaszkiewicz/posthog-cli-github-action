a9bb51090b7ed6a14f22f21f69c64dfa616faec3
ablaszkiewicz <kqmdjc8@gmail.com>
2025-09-10 12:39:13 +0200

    fix: remove everything but action

--- src/some-other-file-m.ts
+++
@@ -1,8 +0,0 @@
-export function otherFileFunctionUpdateCustomProject() {
-  console.log("otherFileFunctionCustomProject");
-  throw new RangeError("otherFileFunctionUpdateCustomProject");
-}
-
-export function someOtherNewFunctionIDidSomeChanges() {
-  console.log("test");
-}
--- update.sh
+++
@@ -1,3 +0,0 @@
-npm run build
-posthog-cli --host http://localhost:8010 sourcemap inject --directory ./dist
-posthog-cli --host http://localhost:8010 sourcemap upload --directory ./dist  --project custom-project --version ver-A
